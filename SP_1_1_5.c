6114900c450fac224419cafe481968fb53ef6bcf
sarthakpatel12 <sarthakp.patel567@gmail.com>
2025-08-21 23:54:36 +0530

    Add files via upload

---
+++ SP_1_1_5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    c=a-b;
+    c=a*b;
+    c=a/b;
+    printf("the addition,subtraction,multiplication,division of the two numbers is :%d",c);
+}
