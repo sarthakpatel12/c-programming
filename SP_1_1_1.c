03d8ca6f123272f146a06a39cdc006e5fad81057
sarthakpatel12 <sarthakp.patel567@gmail.com>
2025-08-21 23:56:39 +0530

    Add files via upload

---
+++ SP_1_1_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    /*program to add 2 Nos.*/
+    int a,b,c;
+
+    printf("Enter 2 Nos.");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("%d+%d=%d\n",a,b,c);
+
+
+}
