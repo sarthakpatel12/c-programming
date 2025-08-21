076bbeee6e2079d9faf11099626ee29a3f0a6dca
sarthakpatel12 <sarthakp.patel567@gmail.com>
2025-08-21 23:52:25 +0530

    Add files via upload

---
+++ SP_1_1_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter two number");
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    printf("the multiplition of the two number is :%d",c);
+}
