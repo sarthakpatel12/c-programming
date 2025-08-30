3e2628ed10c59630cdc92447d635cf9e3f507f29
sarthakpatel12 <sarthakp.patel567@gmail.com>
2025-08-31 00:15:22 +0530

    Add files via upload

---
+++ SP1_1_10_SARTHAK PATEL.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    float dollars,pounds,rupees;
+
+
+    printf("enter the amount of dollars: ");
+    scanf("%f", &dollars);
+
+    rupees = dollars * 48;
+    pounds = rupees / 70;
+
+    printf("converted pounds= %.2f\n", pounds);
+
+    return 0 ;
+}
+
